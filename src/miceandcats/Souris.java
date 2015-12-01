cfff03d75d069f2d686191198824802c0b8cb1d2
Bruno <lapierre.bruno@gmail.com>
2015-12-01 16:17:39 +0100

    direction suite

--- src/miceandcats/Souris.java
+++ src/miceandcats/Souris.java
@@ -8,28 +8,50 @@ package miceandcats;
 public class Souris extends Animal{
 
     public Souris(int id, Case c) {
-        super(id, c);
         
+        super(id, c);
         this.setDirection(directions[1]);
+        indiceDeDirection = 1; 
+        
     }
 
     @Override
     public void Deplacement() 
     {
-         Case myCase = this.getCasee();
+        
+        
+        String myDirection = this.getDirection();
+        Case myCase = this.getCasee();
+        
+        
+        TypeCase myTypeCase = myCase.getType();
+        
+        // retourne l'indice du tableau de direction
+        int myDirectionIndice = retournIndiceDirection(myDirection);
+         
          //Case myNextCase = this.getCasee();
           
          // tester ou recuperer la prochaine case 
-         myCase.setPositionC(myCase.getPositionC() + 1); 
-         
-         
-         switch(myCase.getType())
+         //myCase.setPositionC(myCase.getPositionC() + 1); 
+     
+        
+        
+        
+        
+        
+         switch(myTypeCase)
          {
             
             // deplacement haut
             case MUR:
-            myCase.setPositionC(myCase.getPositionL());
+            
+            myCase.setPositionC(myCase.getPositionC());
             myCase.setPositionC(myCase.getPositionL() + 1);
+            this.setCasee(myCase);
+            
+            myDirectionIndice = (myDirectionIndice + 1)%4; 
+            
+            this.setDirection(myDirectionIndice);
             break; 
              
             // deplacement haut
@@ -58,6 +80,51 @@ public class Souris extends Animal{
             
          }
          
+    }
+    
+    
+    public int retournIndiceDirection(String d)
+    {
+        boolean trouve = false;
+        int indice = 0;
+        int i = 0;
+        
+        while(!trouve && i<directions.length)
+        {
+            if(d.equals(directions[i]))
+            {
+                trouve = true;
+                indice = i;
+            }
+            i++;
+        }
+        
+        return indice;
+        
+    }
+    
+    
+    public static void main(String[] args)
+    {
+        
+        Case c0 = new Case(TypeCase.VIDE, 0,0);
+        Case c1 = new Case(TypeCase.VIDE, 0,1);
+        Case c2 = new Case(TypeCase.VIDE, 0,2);
+        Case c3 = new Case(TypeCase.VIDE, 1,0);
+        Case c4 = new Case(TypeCase.VIDE, 1,1);
+        Case c5 = new Case(TypeCase.VIDE, 1,2);
+        Case c6 = new Case(TypeCase.VIDE, 2,0);
+        Case c7 = new Case(TypeCase.VIDE, 2,1);
+        Case c8 = new Case(TypeCase.VIDE, 2,2);
+       
+        
+        Souris s1 = new Souris(1,c4);
+        
+        
+        
+        
+        
+        
     }
     
 }
